2751bc9af88548ff882122ef9056b1a36324e7cc
Tarzesher <Tarzesher@gmail.com>
2013-04-16 09:17:50 +0200

    Starting the waku shop e-commerce site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WakuShop::Application.config.secret_token = '1c4291ce3338f37db629e8d5bd0df1702f0507aae1b2e8c18c01cd39825028745281978cb3bbd771b1c859323dc72b1a0c5141478dc92a07a12b5994adb0acc6'
