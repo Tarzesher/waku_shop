b5571778651d05d8cfc9e2ef9f3e0fe9d81493db
Tarzesher <Tarzesher@gmail.com>
2013-04-19 10:51:19 +0200

    Custom home page updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def full_title(page_title)
+	generic_title = 'Waku Shop: Online Shopping'
+		if page_title.empty?
+			generic_title
+		else 
+			"#{generic_title} | #{page_title}"
+		end
+
+	end
 end
